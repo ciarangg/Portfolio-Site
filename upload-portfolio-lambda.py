af6d40ddde39afc54c5da59f55609e2aa3c92e9e
Ciaran <cguhagil@terpmail.umd.edu>
2019-01-15 13:49:31 -0700

    added portfolio deployment function for lambda

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,27 @@
+import json
+import boto3
+from botocore.client import Config
+import StringIO
+import zipfile
+
+def lambda_handler(event, context):
+    # TODO implement
+    
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+    portfolio_bucket = s3.Bucket('ciarancod.es')
+    build_bucket = s3.Bucket('portfoliobuild.ciarancod.es')
+
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj,nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
