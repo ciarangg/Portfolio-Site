d3cb77e70640f7e259c39cf29f00f50ed00e0f97
Ciaran <cguhagil@terpmail.umd.edu>
2019-01-15 17:34:37 -0700

    added sns and codepipeline functionality to lambda function. testing CI/CD

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,4 +1,5 @@
 import json
+
 import boto3
 from botocore.client import Config
 import StringIO
@@ -7,19 +8,50 @@ import zipfile
 def lambda_handler(event, context):
     # TODO implement
     
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:789094691283:deployPortfolioTopic')
+    
+    try:
+    
+        job = event.get("CodePipeline.job")
+        
+        location = {
+            "bucketName": 'portfoliobuild.ciarancod.es',
+            "objectKey": 'portfoliobuild.zip'
+        }
+        
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+        
+        print "Building portfolio from" + str(location)
+        
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    
+        portfolio_bucket = s3.Bucket('ciarancod.es')
+        build_bucket = s3.Bucket(location["bucketName"])
+    
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
+    
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+            
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed",
+          Message="Portfolio was not deployed successfully.")
+        raise
 
-    portfolio_bucket = s3.Bucket('ciarancod.es')
-    build_bucket = s3.Bucket('portfoliobuild.ciarancod.es')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
     
     return {
         'statusCode': 200,
